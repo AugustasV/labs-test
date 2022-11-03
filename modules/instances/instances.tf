644f6e103dae0a00a38114ae327d1250ea074637
AugustasV <reg1nt1z@gmail.com>
2022-11-03 11:54:58 +0000

    first commit

---
+++ modules/instances/instances.tf
@@ -0,0 +1,40 @@
+resource "google_compute_instance" "tf-instance-1" {
+  name         = "tf-instance-1"
+  machine_type = "n1-standard-2"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+  metadata_startup_script = <<-EOT
+          #!/bin/bash
+      EOT
+  allow_stopping_for_update = true
+  network_interface {
+ network = "tf-vpc-707736"
+  subnetwork = "subnet-01"
+  }
+}
+
+resource "google_compute_instance" "tf-instance-2" {
+  name         = "tf-instance-2"
+  machine_type = "n1-standard-2"
+  zone         = var.zone
+
+  metadata_startup_script = <<-EOT
+          #!/bin/bash
+      EOT
+  allow_stopping_for_update = true
+boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+network = "tf-vpc-707736" 
+subnetwork = "subnet-02"
+  }
+}
---
+++ modules/instances/variables.tf
@@ -0,0 +1,3 @@
+variable "zone" {
+ default = "ZONE"
+}
